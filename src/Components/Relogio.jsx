63f8cf337073cb1500b4fa893b96564b42078cdc
Stefany3108 <163579112+Stefany3108@users.noreply.github.com>
2024-05-23 03:37:22 -0300

    Add files via upload

---
+++ src/Components/Relogio.jsx
@@ -0,0 +1,35 @@
+
+import { useEffect, useState } from "react";
+
+export default function Relogio() {
+   function getTime() {
+      const today = new Date();
+      const hours = today.getHours();
+      const minutes = today.getMinutes();
+      const seconds = today.getSeconds();
+      return {
+         hours,
+         minutes,
+         seconds,
+      };
+   }
+
+   const [time, setTime] = useState(getTime());
+
+   useEffect(() => {
+      const interval = setInterval(() => {
+         const currentTime = getTime();
+         setTime(currentTime);
+      }, 1000);
+
+      return () => clearInterval(interval);
+   }, []);
+
+   return (
+      <>
+         <div className="relogio">
+            {time.hours}:{time.minutes}:{time.seconds}
+         </div>
+      </>
+   );
+}
